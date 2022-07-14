0912fda410a15ff290c3473cc081af42e028886b
milan poudel <milanwebdeveloper1@gmail.com>
2022-07-14 20:03:00 +0545

    feature/update-delete-a-post-api

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { createPost } = require("../controllers/post");
+const { createPost, updatePost } = require("../controllers/post");
 const router = express.Router();
 
 router.post("/post", createPost);
+router.put("/post/:id", updatePost);
 
 module.exports = router;
