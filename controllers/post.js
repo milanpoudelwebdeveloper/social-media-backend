4587c1a2c5e46cf47e2b60374f33d01db192cf45
milan poudel <milanwebdeveloper1@gmail.com>
2022-07-15 09:47:33 +0545

    feature/like-dislike-a-post

--- controllers/post.js
+++ controllers/post.js
@@ -11,6 +11,18 @@ exports.createPost = async (req, res) => {
   }
 };
 
+//get a single post details
+exports.getPost = async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const post = await Post.find({ _id: postId });
+    res.status(200).json(post);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something went wrong while getting post", e);
+  }
+};
+
 exports.updatePost = async (req, res) => {
   const postId = req.params.id;
   const { userId } = req.body;
@@ -50,5 +62,27 @@ exports.deletePost = async (req, res) => {
   } catch (e) {
     console.log(e);
     res.status(500).send("Something went wrong while deleting post", e);
+  }
+};
+
+//like-dislike a post
+
+exports.likePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+  try {
+    const post = await Post.findById(postId);
+    //we have to check if a user has liked the post, if not liked we will make they like it when they press on button
+    if (!post.likes.includes(userId)) {
+      await Post.findByIdAndUpdate(postId, { $push: { likes: userId } });
+      return res.status(200).json("Post liked successfully");
+    } else {
+      //if user presses on the button and if they have liked it before we have to make them dislike it
+      await Post.findByIdAndUpdate(postId, { $pull: { likes: userId } });
+      return res.status(200).json("Post disliked successfully");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something went wrong while liking post", e);
   }
 };
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { createPost, updatePost } = require("../controllers/post");
+const {
+  createPost,
+  updatePost,
+  getPost,
+  deletePost,
+  likePost,
+} = require("../controllers/post");
 const router = express.Router();
 
 router.post("/post", createPost);
+router.get("/post/:id", getPost);
 router.put("/post/:id", updatePost);
+router.delete("/post/:id", deletePost);
+router.put("/post/like/:id", likePost);
 
 module.exports = router;
